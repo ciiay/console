3e10c4fdeb1df31a39dc8c02534f8c2de721fac7
ciiay <yicai@redhat.com>
2021-10-27 16:31:36 -0400

    GITOPS-1279 abstracted renderResource function to its own component

--- frontend/packages/gitops-plugin/src/components/details/GitOpsResourcesSection.tsx
+++ frontend/packages/gitops-plugin/src/components/details/GitOpsResourcesSection.tsx
@@ -1,22 +1,10 @@
 import * as React from 'react';
-import {
-  Stack,
-  StackItem,
-  Split,
-  SplitItem,
-  Card,
-  CardBody,
-  Tooltip,
-} from '@patternfly/react-core';
-import { HeartBrokenIcon, ExclamationTriangleIcon } from '@patternfly/react-icons';
-import {
-  global_danger_color_100 as RedColor,
-  global_warning_color_100 as YellowColor,
-} from '@patternfly/react-tokens';
+import { Stack, StackItem, Split, SplitItem, Card, CardBody } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
 import { ResourceIcon } from '@console/internal/components/utils';
 import { GitOpsEnvironmentService, GitOpsHealthResources } from '../utils/gitops-types';
 import './GitOpsResourcesSection.scss';
+import GitOpsResourceRow from './GitOpsResourceRow';
 
 interface GitOpsResourcesSectionProps {
   services: GitOpsEnvironmentService[];
@@ -74,49 +62,6 @@ const GitOpsResourcesSection: React.FC<GitOpsResourcesSectionProps> = ({
     ? clusterRoleBindings.reduce(getNonSyncedResources(), [])
     : [];
 
-  const renderResource = (
-    resources: GitOpsHealthResources[] | GitOpsEnvironmentService[],
-    degradedResources: string[] | null,
-    nonSyncedResources: string[],
-  ) => {
-    return (
-      <Split hasGutter>
-        {degradedResources?.length > 0 && (
-          <Tooltip
-            content={t('gitops-plugin~{{x}} of {{total}} Unhealthy', {
-              x: degradedResources.length.toString(),
-              total: resources?.length.toString() ?? '0',
-            })}
-          >
-            <SplitItem>
-              <>
-                {degradedResources.length}
-                &nbsp;
-                <HeartBrokenIcon color={RedColor.value} className="co-icon-space-r" />
-              </>
-            </SplitItem>
-          </Tooltip>
-        )}
-        {nonSyncedResources.length > 0 && (
-          <Tooltip
-            content={t('gitops-plugin~{{x}} of {{total}} OutOfSync', {
-              x: nonSyncedResources.length.toString(),
-              total: resources?.length.toString() ?? '0',
-            })}
-          >
-            <SplitItem>
-              <>
-                {nonSyncedResources.length}
-                &nbsp;
-                <ExclamationTriangleIcon color={YellowColor.value} className="co-icon-space-r" />
-              </>
-            </SplitItem>
-          </Tooltip>
-        )}
-        {degradedResources?.length === 0 && nonSyncedResources.length === 0 && <>&nbsp;</>}
-      </Split>
-    );
-  };
   return (
     <>
       <StackItem className="odc-gitops-resources">
@@ -167,13 +112,41 @@ const GitOpsResourcesSection: React.FC<GitOpsResourcesSectionProps> = ({
               </span>
               <SplitItem>
                 <Stack style={{ alignItems: 'flex-end' }}>
-                  {renderResource(deployments, degradedDeployments, nonSyncedDeployments)}
-                  {renderResource(secrets, degradedSecrets, nonSyncedSecrets)}
-                  {renderResource(services, degradedServices, nonSyncedSyncServices)}
-                  {renderResource(routes, degradedRoutes, nonSyncedRoutes)}
-                  {renderResource(roleBindings, null, nonSyncedRoleBindings)}
-                  {renderResource(clusterRoles, null, nonSyncedClusterRoles)}
-                  {renderResource(clusterRoleBindings, null, nonSyncedClusterRoleBindings)}
+                  <GitOpsResourceRow
+                    resources={deployments}
+                    degradedResources={degradedDeployments}
+                    nonSyncedResources={nonSyncedDeployments}
+                  />
+                  <GitOpsResourceRow
+                    resources={secrets}
+                    degradedResources={degradedSecrets}
+                    nonSyncedResources={nonSyncedSecrets}
+                  />
+                  <GitOpsResourceRow
+                    resources={services}
+                    degradedResources={degradedServices}
+                    nonSyncedResources={nonSyncedSyncServices}
+                  />
+                  <GitOpsResourceRow
+                    resources={routes}
+                    degradedResources={degradedRoutes}
+                    nonSyncedResources={nonSyncedRoutes}
+                  />
+                  <GitOpsResourceRow
+                    resources={roleBindings}
+                    degradedResources={null}
+                    nonSyncedResources={nonSyncedRoleBindings}
+                  />
+                  <GitOpsResourceRow
+                    resources={clusterRoles}
+                    degradedResources={null}
+                    nonSyncedResources={nonSyncedClusterRoles}
+                  />
+                  <GitOpsResourceRow
+                    resources={clusterRoleBindings}
+                    degradedResources={null}
+                    nonSyncedResources={nonSyncedClusterRoleBindings}
+                  />
                 </Stack>
               </SplitItem>
             </Split>
